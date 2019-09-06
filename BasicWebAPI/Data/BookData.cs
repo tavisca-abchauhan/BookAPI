1b8622e3121370ce85d3a97313eb94ac8cd2e002
tavisca-abchauhan <51966236+tavisca-abchauhan@users.noreply.github.com>
2019-09-06 11:47:45 +0530

    Done with Get Post

--- BasicWebAPI/Data/BookData.cs
+++ BasicWebAPI/Data/BookData.cs
@@ -8,18 +8,17 @@ namespace BasicWebAPI.Data
 {
     public class BookData
     {
-         public static List<Book> booklist;
-        public BookData()
+         public static List<Book> booklist=null;
+
+        public static List<Book> GetBookStore()
         {
-            booklist = new List<Book>() {
-            new Book { name = "CleanCode1", price = 100, pages = 350, author = "NoOne", id = 1 },
-            new Book { name = "CleanCode2", price = 200, pages = 400, author = "NoOne", id = 2 },
-            new Book { name = "CleanCode3", price = 300, pages = 450, author = "NoOne", id = 3 }
-                                        };
+            if (booklist == null)
+                booklist = new List<Book>() { new Book { name = "CleanCode1", price = 100, pages = 350, author = "NoOne", id = 1 } };
+            return booklist;
         }
         public void AddBook(Book book)
         {
-            booklist = getlist();
+            booklist = GetBookStore();
             if (book.id>=0&&book.price>=0&& book.name.All(Char.IsLetter)&& book.author.All(Char.IsLetter))
             booklist.Add(book);           
         }
