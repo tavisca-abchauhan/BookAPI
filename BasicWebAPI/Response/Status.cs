cb5e32d965738a13aeae25b4051f73cb2d816fb5
tavisca-abchauhan <51966236+tavisca-abchauhan@users.noreply.github.com>
2019-09-13 00:51:08 +0530

    Add files via upload

---
+++ BasicWebAPI/Response/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicWebAPI.Response
+{
+    public class Status
+    {
+        public int statusCode;
+        public string statusMessage;
+    }
+}
